d4ead3d7001cbc5989288aca31e477b01fecbb51
Luiz A. T. de Arruda <arrudaluiz89@gmail.com>
2023-05-25 22:58:21 -0300

    feat: add function to move task buttons

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,74 @@ const closeTaskModalButton = document.querySelector('#close-task-modal-button');
 const taskForm = document.querySelector('#task-form');
 const taskTitleInput = document.querySelector('#title');
 const taskDescriptionInput = document.querySelector('#description');
+
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 const toDo = 'to do';
 const doing = 'doing';
 const done = 'done';
 
-if (localStorage.getItem('tasks')) {
+function createTask(task) {
+  const taskElement = document.createElement('div');
+  const taskBody = `
+    <button class="expand active">
+      <span>Ler descrição</span>
+      <span aria-hidden="true" class="material-icons icon">expand_more</span>
+    </button>
+    <button class="expand highlighted">
+      <span>Esconder descrição</span>
+      <span aria-hidden="true" class="material-icons icon">expand_less</span>
+    </button>
+    <p class="description">${task.description}</p>`;
+  const taskMoveButton = {
+    back: `
+      <button class="button back" aria-label="Retroceder tarefa">
+        <span aria-hidden="true" class="material-icons icon">navigate_before</span>
+      </button>`,
+    forward: `
+      <button class="button forward" aria-label="Avançar tarefa">
+        <span aria-hidden="true" class="material-icons icon">navigate_next</span>
+      </button>`,
+    restart: `
+      <button class="button restart" aria-label="Reiniciar tarefa">
+        <span aria-hidden="true" class="material-icons icon">replay</span>
+      </button>`
+  };
+  const taskContent = `
+    <div class="header">
+      <h3 class="title">${task.title}</h3>
+      <button class="more-button" aria-label="Abrir menu da tarefa">
+        <span aria-hidden="true" class="material-icons icon">more_vert</span>
+      </button>
+      <div class="menu">
+        <button class="option-button delete-button">
+          <span aria-hidden="true" class="material-icons icon">delete_outline</span>
+          <span>Excluir</span>
+        </button>
+      </div>
+    </div>
+    <div class="body">
+      <div class="task-info">
+        ${task.description === '' ? '' : taskBody}
+      </div>
+      <div class="task-move">
+        ${
+          task.status === toDo
+            ? taskMoveButton.forward
+            : task.status === doing
+            ? taskMoveButton.back + taskMoveButton.forward
+            : taskMoveButton.back + taskMoveButton.restart
+        }
+      </div>
+    </div>`;
+
+  taskElement.setAttribute('id', task.id);
+  taskElement.setAttribute('class', 'task');
+  taskElement.insertAdjacentHTML('afterbegin', taskContent);
+
+  return taskElement;
+}
+
+function updateListBoard() {
   tasks.map((task) => {
     const taskElement = createTask(task);
     switch (task.status) {
@@ -43,62 +105,6 @@ if (localStorage.getItem('tasks')) {
   });
 }
 
-function createTask(task) {
-  const taskElement = document.createElement('div');
-  const taskMoveButton = {
-    back: `<button class="button back" aria-label="Retroceder tarefa">
-            <span aria-hidden="true" class="material-icons icon">navigate_before</span>
-          </button>`,
-    forward: `<button class="button forward" aria-label="Avançar tarefa">
-            <span aria-hidden="true" class="material-icons icon">navigate_next</span>
-          </button>`,
-    restart: `<button class="button restart" aria-label="Reiniciar tarefa">
-            <span aria-hidden="true" class="material-icons icon">replay</span>
-          </button>`
-  };
-  const taskContent = `
-    <div class="header">
-      <h3 class="title">${task.title}</h3>
-      <button class="more-button" aria-label="Abrir menu da tarefa">
-        <span aria-hidden="true" class="material-icons icon">more_vert</span>
-      </button>
-      <div class="menu">
-        <button class="option-button delete-button">
-          <span aria-hidden="true" class="material-icons icon">delete_outline</span>
-          <span>Excluir</span>
-        </button>
-      </div>
-    </div>
-    <div class="body">
-      <div class="task-info">
-        <button class="expand active">
-          <span>Ler descrição</span>
-          <span aria-hidden="true" class="material-icons icon">expand_more</span>
-        </button>
-        <button class="expand highlighted">
-          <span>Esconder descrição</span>
-          <span aria-hidden="true" class="material-icons icon">expand_less</span>
-        </button>
-        <p class="description">${task.description}</p>
-      </div>
-      <div class="task-move">
-      ${
-        task.status === toDo
-          ? taskMoveButton.forward
-          : task.status === doing
-          ? taskMoveButton.back + taskMoveButton.forward
-          : taskMoveButton.back + taskMoveButton.restart
-      }
-      </div>
-    </div>`;
-
-  taskElement.setAttribute('id', task.id);
-  taskElement.setAttribute('class', 'task');
-  taskElement.insertAdjacentHTML('afterbegin', taskContent);
-
-  return taskElement;
-}
-
 function addTask(event) {
   event.preventDefault();
 
@@ -118,11 +124,73 @@ function addTask(event) {
   taskModal.classList.remove('modal-active');
 }
 
-function removeTask(id) {
+function moveTaskBack(id) {
+  const index = tasks.findIndex((task) => task.id === id);
+  let taskElement = null;
+  document.querySelector('#' + id).remove();
+
+  switch (tasks[index].status) {
+    case done:
+      tasks[index].status = doing;
+      taskElement = createTask(tasks[index]);
+      doingList.appendChild(taskElement);
+      break;
+
+    case doing:
+      tasks[index].status = toDo;
+      taskElement = createTask(tasks[index]);
+      todoList.appendChild(taskElement);
+      break;
+
+    default:
+      console.error('Task ' + tasks[index].title + ' was not handled');
+      break;
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function moveTaskForward(id) {
+  const index = tasks.findIndex((task) => task.id === id);
+  let taskElement = null;
+  document.querySelector('#' + id).remove();
+
+  switch (tasks[index].status) {
+    case toDo:
+      tasks[index].status = doing;
+      taskElement = createTask(tasks[index]);
+      doingList.appendChild(taskElement);
+      break;
+
+    case doing:
+      tasks[index].status = done;
+      taskElement = createTask(tasks[index]);
+      doneList.appendChild(taskElement);
+      break;
+
+    case done:
+      tasks[index].status = toDo;
+      taskElement = createTask(tasks[index]);
+      todoList.appendChild(taskElement);
+      break;
+
+    default:
+      console.error('Task ' + tasks[index].title + ' was not handled');
+      break;
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function deleteTask(id) {
   const index = tasks.findIndex((task) => task.id === id);
   tasks.splice(index, 1);
   localStorage.setItem('tasks', JSON.stringify(tasks));
   document.querySelector('#' + id).remove();
+}
+
+if (localStorage.getItem('tasks')) {
+  updateListBoard();
 }
 
 /**
@@ -166,32 +234,51 @@ closeTaskModalButton.addEventListener('click', () => {
 
 taskForm.addEventListener('submit', addTask);
 
-document.addEventListener('click', (event) => {
+listBoard.addEventListener('click', (event) => {
   if (
+    event.target.classList.contains('back') ||
+    event.target.parentElement.classList.contains('back')
+  ) {
+    /** Move task back button */
+    const taskId = event.target.closest('.task').id;
+    moveTaskBack(taskId);
+  } else if (
+    event.target.classList.contains('forward') ||
+    event.target.parentElement.classList.contains('forward') ||
+    event.target.classList.contains('restart') ||
+    event.target.parentElement.classList.contains('restart')
+  ) {
+    /** Move task forward button */
+    const taskId = event.target.closest('.task').id;
+    moveTaskForward(taskId);
+  } else if (
     event.target.classList.contains('more-button') ||
     event.target.parentElement.classList.contains('more-button')
   ) {
+    /** Task menu button */
     const taskId = event.target.closest('.task').id;
-    document.querySelector(`#${taskId} .menu`).classList.toggle('active');
-  }
-
-  if (
+    listBoard.querySelector(`#${taskId} .menu`).classList.toggle('active');
+  } else if (
     event.target.classList.contains('delete-button') ||
     event.target.parentElement.classList.contains('delete-button')
   ) {
+    /** Delete task button */
     const taskId = event.target.closest('.task').id;
-    document.querySelector(`#${taskId} .menu`).classList.remove('active');
-    removeTask(taskId);
+    listBoard.querySelector(`#${taskId} .menu`).classList.remove('active');
+    deleteTask(taskId);
   }
 });
 
-const taskInfos = document.querySelectorAll('.task-info');
-taskInfos.forEach((taskInfo) =>
-  taskInfo.addEventListener('click', () => {
-    const expandButtons = taskInfo.querySelectorAll('.expand');
-    expandButtons.forEach((expandButton) =>
-      expandButton.classList.toggle('active')
-    );
-    taskInfo.querySelector('.description').classList.toggle('active');
-  })
-);
+const taskInfos = listBoard.querySelectorAll('.task-info');
+taskInfos.forEach((taskInfo) => {
+  if (taskInfo.firstElementChild) {
+    console.log(taskInfo.firstElementChild);
+    taskInfo.addEventListener('click', () => {
+      const expandButtons = taskInfo.querySelectorAll('.expand');
+      expandButtons.forEach((expandButton) =>
+        expandButton.classList.toggle('active')
+      );
+      taskInfo.querySelector('.description').classList.toggle('active');
+    });
+  }
+});
